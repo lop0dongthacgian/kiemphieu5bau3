34cc2806d91ca5ca0027047357afc86a71603cd6
lop0dongthacgian <lop0dongthacgian@gmail.com>
2026-01-22 19:27:56 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+// CẤU HÌNH ỨNG VIÊN - CHỈ SỬA Ở ĐÂY
+const candidates = {
+  'A': 'Nguyễn Văn Anh',
+  'B': 'Trần Kim Bá', 
+  'C': 'Hoàng Thị Cê',
+  'D': 'Lê Tấn Dũng',
+  'E': 'Bùi Thị Thanh Em'
+};
+
+// Không cần sửa phần dưới đây
+const candidateKeys = Object.keys(candidates);
+const candidateCount = candidateKeys.length;
